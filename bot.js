e476a3a2e26fbd5301f6139131a2be0744035936
Rezfix <44650563+Rezfix@users.noreply.github.com>
2019-01-30 19:59:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const gif = require("gif-search");
 const client = new Discord.Client({disableEveryone: true});
 
 client.on('ready', () => {
-    client.user.setGame('Type 1help','');
+    client.user.setGame('Type 1help','https://www.twitch.tv/Tune');
     console.log('---------------');
     console.log('Tune 1Play')
     console.log('---------------')
